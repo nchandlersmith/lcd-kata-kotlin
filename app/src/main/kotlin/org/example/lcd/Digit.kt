6a03ae9092d58bd302cf93298a45a3f6ec63a4d1
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2024-11-09 09:06:18 -0600

    Refactor

--- app/src/main/kotlin/org/example/lcd/Digit.kt
+++ app/src/main/kotlin/org/example/lcd/Digit.kt
@@ -1,23 +1,49 @@
 package org.example.lcd
 
+val emptyLcd: Array<Array<Char>> = arrayOf(emptyArray())
+
 fun toDigit(number: Int): Array<Array<Char>> {
-    if (3 == number) {
-        return arrayOf(
-            arrayOf(' ', '_', ' '),
-            arrayOf(' ', '_', '|'),
-            arrayOf(' ', '_', '|')
-        )
+    val rules = listOf(digitRule1, digitRule2, digitRule3)
+    var lcdDigit = emptyLcd
+    for (rule in rules) {
+        lcdDigit = rule(number)
+        if (!lcdDigit.first().isEmpty()) {
+            return lcdDigit
+        }
     }
-    if (2 == number) {
-        return arrayOf(
-            arrayOf(' ', '_', ' '),
-            arrayOf(' ', '_', '|'),
-            arrayOf('|', '_', ' ')
-        )
-    }
-    return arrayOf(
+    return lcdDigit
+}
+
+private val digitRule3: (Int) -> Array<Array<Char>> = { number ->
+    val digitLcd = arrayOf(
+        arrayOf(' ', '_', ' '),
+        arrayOf(' ', '_', '|'),
+        arrayOf(' ', '_', '|')
+    )
+    digitRule(number, 3, digitLcd)
+}
+
+private val digitRule2: (Int) -> Array<Array<Char>> = { number ->
+    val digitLcd = arrayOf(
+        arrayOf(' ', '_', ' '),
+        arrayOf(' ', '_', '|'),
+        arrayOf('|', '_', ' ')
+    )
+    digitRule(number, 2, digitLcd)
+}
+
+private val digitRule1: (Int) -> Array<Array<Char>> = { number ->
+    val digitLcd = arrayOf(
         arrayOf(' ', ' ', ' '),
         arrayOf(' ', '|', ' '),
         arrayOf(' ', '|', ' ')
     )
+    digitRule(number, 1, digitLcd)
+}
+
+private fun digitRule(number: Int, digit: Int, digitLcd: Array<Array<Char>>): Array<Array<Char>> {
+    if (number == digit) {
+        return digitLcd
+    }
+    return emptyLcd
 }
